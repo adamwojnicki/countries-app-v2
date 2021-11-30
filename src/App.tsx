8efb86a897e3399c3571a8cfd3aeff9009b262e9
Adam Wojnicki <wojnicki.ada1@gmial.com>
2021-11-30 21:53:01 +0100

    Importing layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Main } from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      dupa
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
